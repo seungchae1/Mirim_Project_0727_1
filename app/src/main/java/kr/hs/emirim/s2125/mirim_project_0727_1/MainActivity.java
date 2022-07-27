416fefe1ef36544ad57042bb1cec9a322b0f9f58
seungchae1 <s2125@e-mirim.hs.kr>
2022-07-27 09:27:49 +0900

    체크박스 목록 대화상자

--- app/src/main/java/kr/hs/emirim/s2125/mirim_project_0727_1/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/s2125/mirim_project_0727_1/MainActivity.java
@@ -1,14 +1,31 @@
 package kr.hs.emirim.s2125.mirim_project_0727_1;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btnCheck;
+    String[] itemsArr = {"Italy", "France", "Spain"};
+    boolean[] checkArr={true, false, true}; //처음 실행될 때 초기상태 설정
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnCheck = findViewById(R.id.btn_check);
+        btnCheck.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder dlg = new AlertDialog.Builder(MainActivity.this);
+                dlg.setTitle("체크목록대화상자");
+                dlg.setIcon(R.drawable.icon);
+                dlg.setMultiChoiceItems(itemsArr, checkArr, null);
+                dlg.setNegativeButton("닫기",null);
+                dlg.show();
+            }
+        });
     }
 }
